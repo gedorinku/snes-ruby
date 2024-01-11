e200a313ba9d546fbb82b30723d5af91250356e2
Ryota Egusa <egusa.ryota@gmail.com>
2024-01-11 22:01:41 +0900

    Add

---
+++ Makefile
@@ -0,0 +1,26 @@
+PVSNESLIB_HOME=pvsneslib
+PVSNESLIB_DEBUG=1
+
+CFLAGS += -Isrc/mrubyc -Isrc/musl -D MRBC_USE_FLOAT=0
+
+include ${PVSNESLIB_HOME}/devkitsnes/snes_rules
+
+.PHONY: bitmaps all
+
+#---------------------------------------------------------------------------------
+# ROMNAME is used in snes_rules file
+export ROMNAME := hello_world
+
+all: src/main.rb.bytecode.c bitmaps $(ROMNAME).sfc
+
+clean: cleanBuildRes cleanRom cleanGfx
+	
+#---------------------------------------------------------------------------------
+pvsneslibfont.pic: pvsneslibfont.png
+	@echo convert font with no tile reduction ... $(notdir $@)
+	$(GFXCONV) -n -gs8 -po16 -pc16 -pe0 -mR! -m! -fpng $<
+
+bitmaps : pvsneslibfont.pic
+
+src/main.rb.bytecode.c: src/main.rb
+	mrbc --remove-lv -Bmrbbuf -o src/main.rb.bytecode.c $<
---
+++ src/main.c
@@ -0,0 +1,84 @@
+#include <snes.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mrubyc.h"
+
+#include "main.rb.bytecode.c"
+
+#if !defined(MRBC_MEMORY_SIZE)
+// FIXME: 8067 より大きくすると mrbc_init_alloc に失敗する
+#define MRBC_MEMORY_SIZE (8000)
+#endif
+static uint8_t memory_pool[MRBC_MEMORY_SIZE];
+
+extern char tilfont, palfont;
+
+int run() {
+    mrbc_init_alloc(memory_pool, MRBC_MEMORY_SIZE);
+    mrbc_init_global();
+    return 2;
+    mrbc_init_class();
+    
+    mrbc_vm *vm = mrbc_vm_open(NULL);
+    if( vm == NULL ) {
+        consoleDrawText(10, 10, "vm is null");
+        return 1;
+    }
+
+    if( mrbc_load_mrb(vm, mrbbuf) != 0 ) {
+        consoleDrawText(10, 10, "failed to load");
+        return 1;
+    }
+
+    mrbc_vm_begin( vm );
+    int ret = mrbc_vm_run( vm );
+    mrbc_vm_end( vm );
+    mrbc_vm_close( vm );
+
+    return ret;
+}
+
+int main(void)
+{
+    // Initialize SNES
+    consoleInit();
+
+    // Initialize text console with our font
+    consoleSetTextVramBGAdr(0x6800);
+    consoleSetTextVramAdr(0x3000);
+    consoleSetTextOffset(0x0100);
+    consoleInitText(0, 16 * 2, &tilfont, &palfont);
+
+    // Init background
+    bgSetGfxPtr(0, 0x2000);
+    bgSetMapPtr(0, 0x6800, SC_32x32);
+
+    // Now Put in 16 color mode and disable Bgs except current
+    setMode(BG_MODE1, 0);
+    bgSetDisable(1);
+    bgSetDisable(2);
+
+    /*
+        start mruby/c
+    */
+    int ret;
+    ret = run();
+    char res[16];
+    sprintf(res, "ret: %d", ret);
+    consoleDrawText(10, 10, res);
+
+    // Draw a wonderful text :P
+    // consoleDrawText(10, 10, "Hello World !");
+    consoleDrawText(6, 14, "WELCOME TO PVSNESLIB");
+    consoleDrawText(3, 18, "HTTPS://WWW.PORTABLEDEV.COM");
+
+    // Wait for nothing :P
+    setScreenOn();
+
+    while (1)
+    {
+        WaitForVBlank();
+    }
+    return 0;
+}
