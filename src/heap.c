1dcee9b1318c311d39603d52c658be801674238c
Ryota Egusa <egusa.ryota@gmail.com>
2024-01-14 19:35:03 +0900

    Add

---
+++ src/heap.c
@@ -0,0 +1,2 @@
+void * far heap_start = (void*)0x7e0000;
+void * far heap_end = (void*)0x7effff;
---
+++ src/va_copy.h
@@ -0,0 +1,8 @@
+
+#ifndef _VA_COPY_H
+#define _VA_COPY_H
+
+#define va_copy(dest, src) \
+    ((dest) = (src))
+
+#endif
