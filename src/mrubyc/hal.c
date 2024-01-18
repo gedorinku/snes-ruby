36784fa3737f601812662881d2d07ad4b1bd7cf3
Ryota Egusa <egusa.ryota@gmail.com>
2024-01-18 21:18:07 +0900

    Implement hal_write

--- src/mrubyc/hal.c
+++ src/mrubyc/hal.c
@@ -1,6 +1,28 @@
+#include <string.h>
 #include "hal.h"
 
+#define HAL_BUF_SIZE (256)
+static char hal_buf[HAL_BUF_SIZE];
+static int hal_buf_pos = 0;
+
 int hal_write(int fd, const void *buf, int nbytes) {
-    // TODO
+    int l = 0;
+    while (l < nbytes) {
+        int s = HAL_BUF_SIZE - hal_buf_pos;
+        if ((nbytes - l) < s) {
+            s = (nbytes - l);
+        }
+
+        memset(hal_buf + hal_buf_pos, 0, s);
+        memcpy(hal_buf, (const char *)buf + l, s);
+
+        l += s;
+        hal_buf_pos += s;
+
+        if (HAL_BUF_SIZE <= hal_buf_pos) {
+            hal_buf_pos = 0;
+        }
+    }
+
     return 0;
 }
