aab7d3485305c4d8546c2437526dce15c9496a0f
Ryota Egusa <egusa.ryota@gmail.com>
2024-01-20 19:39:22 +0900

    Improve for debug

--- src/musl/assert.h
+++ src/musl/assert.h
@@ -3,7 +3,5 @@
 #ifdef NDEBUG
 #define	assert(x) (void)0
 #else
-// FIXME: assert is not supported
-#define	assert(x) (void)0
-// #define assert(x) ((void)((x) || (__assert_fail(#x, __FILE__, __LINE__, __func__),0)))
+#define assert(x) ((void)((x) || (hal_write(0, #x, strlen(#x)) && exit(1))))
 #endif
