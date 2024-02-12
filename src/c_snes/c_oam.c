44b213a47e5174bd97b8f6461d64eeadedf62628
Ryota Egusa <egusa.ryota@gmail.com>
2024-02-12 20:54:56 +0900

    Add sprite sample (#5)
    * Render sprites
    
    * Move sprites

---
+++ src/c_snes/c_oam.c
@@ -0,0 +1,30 @@
+#include <snes.h>
+
+#include "mrubyc.h"
+
+static void c_snes_oam_set(mrbc_vm *vm, mrbc_value v[], int argc) {
+  oamSet(v[1].i, v[2].i, v[3].i, v[4].i, v[5].i, v[6].i, v[7].i, v[8].i);
+}
+
+static void c_snes_oam_set_ex(mrbc_vm *vm, mrbc_value v[], int argc) {
+  const bool hide = v[3].tt == MRBC_TT_TRUE ? true : false;
+
+  oamSetEx(v[1].i, v[2].i, hide);
+}
+
+static void c_snes_oam_show(mrbc_vm *vm, mrbc_value v[], int argc) {
+  oamSetVisible(v[1].i, OBJ_SHOW);
+}
+
+static void c_snes_oam_hide(mrbc_vm *vm, mrbc_value v[], int argc) {
+  oamSetVisible(v[1].i, OBJ_HIDE);
+}
+
+void snes_init_class_oam(struct VM *vm, mrbc_class *snes_class) {
+  mrbc_class *cls = mrbc_define_class_under(vm, snes_class, "OAM", NULL);
+
+  mrbc_define_method(vm, cls, "set", c_snes_oam_set);
+  mrbc_define_method(vm, cls, "set_ex", c_snes_oam_set_ex);
+  mrbc_define_method(vm, cls, "show", c_snes_oam_show);
+  mrbc_define_method(vm, cls, "hide", c_snes_oam_hide);
+}
--- src/main.rb
+++ src/main.rb
@@ -5,6 +5,7 @@ KEY_LEFT = 512
 
 x = 0
 y = 0
+bg_y = 0
 
 while true
   SNES::Pad.wait_for_scan
@@ -23,7 +24,11 @@ while true
     x -= 2
   end
 
-  SNES::Bg.scroll(1, x, y)
+  SNES::OAM.set(0, x, y, 3, 0, 0, 0, 0)
+
+  bg_y += 2
+
+  SNES::Bg.scroll(1, 0, bg_y)
 
   SNES.wait_for_vblank()
 end
