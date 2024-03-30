a142302f9c503c746e75eaf236965e8bc561162b
Ryota Egusa <egusa.ryota@gmail.com>
2024-03-31 01:40:59 +0900

    Add to update vram

--- src/sa1/c_snes/c_bg.c
+++ src/sa1/c_snes/c_bg.c
@@ -18,7 +18,48 @@ static void c_snes_bg_scroll(mrbc_vm *vm, mrbc_value v[], int argc) {
              (u16)v[3].i);
 }
 
+static u8 *default_tile_maps[4];
+static size_t default_tile_map_sizes[4];
+static u16 *tile_map_vram_addrs[4];
+
+static void c_snes_bg_get_default_tile_map(mrbc_vm *vm, mrbc_value v[],
+                                           int argc) {
+  const int bg = v[1].i;
+  const size_t size = default_tile_map_sizes[bg];
+  mrbc_value res = mrbc_array_new(vm, size);
+
+  int i;
+  for (i = 0; i < size; i++) {
+    mrbc_array_set(&res, i, &mrbc_integer_value(default_tile_maps[bg][i]));
+  }
+
+  SET_RETURN(res);
+}
+
+static void c_snes_bg_update_tile_map(mrbc_vm *vm, mrbc_value v[], int argc) {
+  const int bg = v[1].i;
+  const size_t size = default_tile_map_sizes[bg];
+
+  static char *buf;
+  static size_t buf_size;
+
+  if (buf_size < size) {
+    if (buf != NULL) {
+      sa1_free(buf);
+    }
+
+    buf = sa1_malloc(size);
+    buf_size = size;
+  }
+
+  call_s_cpu(dmaCopyVram, sizeof(char *) + sizeof(u16) * 2, buf,
+             tile_map_vram_addrs[bg], (u16)size);
+}
+
 void snes_init_class_bg(struct VM *vm, mrbc_class *snes_class) {
   mrbc_class *cls = mrbc_define_class_under(vm, snes_class, "Bg", NULL);
   mrbc_define_method(vm, cls, "scroll", c_snes_bg_scroll);
+  mrbc_define_method(vm, cls, "default_tile_maps",
+                     c_snes_bg_get_default_tile_map);
+  mrbc_define_method(vm, cls, "update_tile_map", c_snes_bg_update_tile_map);
 }
