8bdb70e5ddc8f3c416d22ca8d532da938efcf34d
Ryota Egusa <egusa.ryota@gmail.com>
2024-04-04 00:51:36 +0900

    Render blocks

--- src/main.rb
+++ src/main.rb
@@ -6,8 +6,71 @@ KEY_LEFT = 512
 
 MAX_Y = 224 - 16
 
-tile_maps = SNES::Bg.default_tile_maps(1)
-tile_maps[2] = 3
+BLOCK_MAP = [
+  [160, 161, 165],
+  [32, 33, 37],
+  [32, 33, 37],
+]
+
+class BlockPair
+  GAP = 10
+
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def render(tile_maps)
+    gap_tile_x = x / 8
+    gap_tile_y = y / 8
+
+    block_tile_x = gap_tile_x
+    upper_block_tile_y = gap_tile_y - 1
+
+    i = 0
+    while i < BLOCK_MAP.size && 0 <= upper_block_tile_y - i
+      j = 0
+      while j < BLOCK_MAP[i].size
+        tile_maps[32 * (upper_block_tile_y - i) + j + block_tile_x] = BLOCK_MAP[i][j]
+        j += 1
+      end
+      i += 1
+    end
+
+    lower_block_tile_y = gap_tile_y + GAP
+
+    i = 0
+    while i < BLOCK_MAP.size && lower_block_tile_y + i < 32
+      j = 0
+      while j < BLOCK_MAP[i].size
+        tile_maps[32 * (lower_block_tile_y + i) + j + block_tile_x] = BLOCK_MAP[i][j]
+        j += 1
+      end
+      i += 1
+    end
+  end
+end
+
+block_pairs = [
+  BlockPair.new(48, 24),
+  BlockPair.new(96, 40),
+]
+
+tile_maps = Array.new(32 * 32, 0)
+
+def render_block_pairs(block_pairs, tile_maps)
+  i = 0
+  while i < block_pairs.size
+    block_pairs[i].render(tile_maps)
+    i += 1
+  end
+end
+
+render_block_pairs(block_pairs, tile_maps)
+
+SNES::Bg.update_tile_map(1, tile_maps)
 
 x = 0
 y = 0
@@ -42,8 +105,6 @@ while true
   SNES::OAM.set(0, 0, player_y, 3, 0, 0, 0, 0)
 
   SNES::Bg.scroll(1, x, y)
-
-  SNES::Bg.update_tile_map(1, tile_maps)
 
   SNES::SPC.process
   SNES.wait_for_vblank
--- src/snesw.c
+++ src/snesw.c
@@ -6,5 +6,6 @@ void snesw_wait_and_dma_to_vram(const u8 *source, u16 address, u16 size) {
   // DMAはVBlank中でないと動作しない
   WaitForVBlank();
 
+  // TODO: VBlankを待ってるので多分DMA使わずにコピーしたほうが高速
   dmaCopyVram(source, address, size);
 }
